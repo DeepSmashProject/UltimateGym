f205a2a1d9489e7535049882f901db6ea3752311
DeepSmashProject <deepsmash0@gmail.com>
2021-11-16 19:35:31 +0900

    fix screen

--- ultimate_gym/screen.py
+++ ultimate_gym/screen.py
@@ -3,13 +3,13 @@ from yuzulib import Screen as YuzuScreen
 class Screen(YuzuScreen):
     def __init__(self):
         super().__init__(self._callback, fps=2)
-        self.frame = None
-        self.fps = 0
+        self.current_frame = None
+        self.current_fps = 0
 
     def _callback(self, frame, fps):
         #print("callback!", frame[0][0], fps)
-        self.frame = frame
-        self.fps = fps
+        self.current_frame = frame
+        self.current_fps = fps
 
     def get(self):
-        return self.frame, self.fps
+        return self.current_frame, self.current_fps
